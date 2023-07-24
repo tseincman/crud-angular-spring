1e586f4bf0dad4aa18240150ad482d21eb86852c
tseincman <tseincman@gmail.com>
2023-07-24 12:48:29 -0300

    list icon implemented

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { AppMaterialModule } from './app-material/app-material.module';
+import { CategoryPipe } from './pipes/category.pipe';
 
 
 
 @NgModule({
   declarations: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ],
   imports: [
     AppMaterialModule,
     CommonModule
   ],
   exports: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe
   ]
 })
 export class SharedModule { }
